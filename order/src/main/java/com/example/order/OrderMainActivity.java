0dd7f1dfa01345a04e78b4d5934bd7d40e4df568
pingji2016 <3192086411@qq.com>
2021-04-07 16:10:20 +0800

    [android 组件化实战] 添加模块oder,personal,common

---
+++ order/src/main/java/com/example/order/OrderMainActivity.java
@@ -0,0 +1,13 @@
+package com.example.order;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class OrderMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.order_main_activity);
+    }
+}
---
+++ personal/src/main/java/com/example/personal/PersonalMainActivity.java
@@ -0,0 +1,13 @@
+package com.example.personal;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PersonalMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.personal_main_activity);
+    }
+}
