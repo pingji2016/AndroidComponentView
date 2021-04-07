802a18278e3d97687dc71c758ad80b7df57836b6
pingji2016 <3192086411@qq.com>
2021-04-07 16:55:05 +0800

    [android 组件化实战] oder,personal,common界面调转

--- app/src/main/java/com/example/wq/androidcomponentview/MainActivity.java
+++ app/src/main/java/com/example/wq/androidcomponentview/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.wq.androidcomponentview;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.order.OrderMainActivity;
+import com.example.personal.PersonalMainActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void toOrderModule(View view) {
+        startActivity(new Intent(this, OrderMainActivity.class));
+    }
+
+    public void toPersonalModule(View view) {
+        startActivity(new Intent(this, PersonalMainActivity.class));
     }
 }
