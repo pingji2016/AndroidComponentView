25db931e61d08a4ee5511abea79e152f088f16a3
pingji2016 <3192086411@qq.com>
2021-04-21 23:23:44 +0800

    [android 第一行] material Design.添加图片

--- order/src/main/java/com/example/order/OrderMainActivity.java
+++ order/src/main/java/com/example/order/OrderMainActivity.java
@@ -1,7 +1,15 @@
 package com.example.order;
 
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
 
 import com.example.common.NetManagerUtils;
 
@@ -11,5 +19,44 @@ public class OrderMainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.order_main_activity);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        CollapsingToolbarLayout collapsingToolbarLayout = (CollapsingToolbarLayout)
+                findViewById(R.id.coll_toolbar);
+
+        ImageView imgview = (ImageView) findViewById(R.id.img);
+        TextView content_text = (TextView) findViewById(R.id.content_text);
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+
+        if (actionBar == null){
+
+        }else{
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+        collapsingToolbarLayout.setTitle("订单界面");
+        Glide.with(OrderMainActivity.this)
+                .load("http://inthecheesefactory.com/uploads/source/nestedfragment/fragments.png")
+                .into(imgview);
+
+        content_text.setText(gennerateString(566));
+    }
+
+    private String gennerateString(int num){
+        StringBuilder ans = new StringBuilder();
+        for(int i = 0;i<500;i++){
+            ans.append("dgskjhds ds flg sda ");
+        }
+        return ans.toString();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case android.R.id.home:
+                finish();
+                return  true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
